dc52dff5eab100ef35a357039e49fce45d12f2eb
Helena.Han <hlyhelena37@gmail.com>
2024-01-04 21:23:23 +1100

    create login route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Footer from './components/Footer';
+import Login from './pages/Login';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Homepage />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 			<Footer />
 		</BrowserRouter>
