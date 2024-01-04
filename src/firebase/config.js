0e64b2af0f0c4e19099b692d0800fe5f3201d746
Helena.Han <hlyhelena37@gmail.com>
2024-01-04 22:27:41 +1100

    config firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyA7fVurjAA41CH30JktscU2Qy6lrdAEAVU',
+	authDomain: 'yoyo-time-tracker.firebaseapp.com',
+	projectId: 'yoyo-time-tracker',
+	storageBucket: 'yoyo-time-tracker.appspot.com',
+	messagingSenderId: '85060388909',
+	appId: '1:85060388909:web:2bf5873fa3311360fd10a4',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
