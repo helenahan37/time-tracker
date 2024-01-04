19378de62bfd61b7e8bc01f1e6b1a53559be3e8a
Helena.Han <hlyhelena37@gmail.com>
2024-01-04 22:53:13 +1100

    create login and register with firebase

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,51 @@
 import React, { useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { FcGoogle } from 'react-icons/fc';
+import app from '../firebase/config';
+import { useNavigate } from 'react-router';
+
+//create auth instance
+const auth = getAuth(app);
+
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [loginLoading, setLoginLoading] = useState(false);
+	const [registerLoading, setRegisterLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	//navigate instance
+	const navigate = useNavigate();
+
+	// register
+	const signUp = async () => {
+		setRegisterLoading(true);
+		setError(null);
+		try {
+			//make request to firebase
+			createUserWithEmailAndPassword(auth, email, password);
+			alert('Account created successfully');
+			navigate('/reports');
+		} catch (error) {
+			setError(error.message);
+		}
+	};
+
+	// login
+	const logIn = async () => {
+		setLoginLoading(true);
+		setError(null);
+		try {
+			//make request to firebase
+			const user = signInWithEmailAndPassword(auth, email, password);
+			console.log(user);
+			alert('Login successfully');
+			navigate('/reports');
+		} catch (error) {
+			setError(error.message);
+		}
+	};
+
 	return (
 		<div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
 			<div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -20,6 +65,7 @@ const Login = () => {
 									name="email"
 									type="email"
 									autoComplete="email"
+									onChange={(e) => setEmail(e.target.value)}
 									required
 									className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 								/>
@@ -36,6 +82,7 @@ const Login = () => {
 									name="password"
 									type="password"
 									autoComplete="current-password"
+									onChange={(e) => setPassword(e.target.value)}
 									required
 									className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 								/>
@@ -45,11 +92,13 @@ const Login = () => {
 						<div>
 							{/* Error here */}
 							<button
+								onClick={signUp}
 								type="button"
 								className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
 								Register
 							</button>
 							<button
+								onClick={logIn}
 								type="button"
 								className="w-full flex mt-3 justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-pink-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
 								Login
