00a844743e2c7fe549fe0415b2cc5164a60f88fb
Helena.Han <hlyhelena37@gmail.com>
2024-01-07 21:02:06 +1100

    render task component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './pages/Homepage';
 import Footer from './components/Footer';
 import Login from './pages/Login';
 import CreateTaskPage from './pages/CreateTask';
+import Reports from './pages/Reports';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<Route path="/" element={<Homepage />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/create-task" element={<CreateTaskPage />} />
+				<Route path="/reports" element={<Reports />} />
 			</Routes>
 			<Footer />
 		</BrowserRouter>
