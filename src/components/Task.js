0f6f2067d7cb537a0ee6bad32b20e233148327be
Helena.Han <hlyhelena37@gmail.com>
2024-01-08 22:17:03 +1100

    create handler

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,6 +4,38 @@ import { BsCircleFill } from 'react-icons/bs';
 import { AiOutlineEdit, AiOutlineDelete, AiOutlineCalendar } from 'react-icons/ai';
 
 function Task({ task }) {
+	const [localState, setLocalState] = useState(task);
+	const [isEditing, setIsEditing] = useState(false);
+	const [newTaskDescription, setNewTaskDescription] = useState(localState.task);
+	//handle editing
+	const handleEdit = () => {
+		setIsEditing(true);
+	};
+
+	//handle cancel edit
+	const handleCancelEdit = () => {
+		setIsEditing(false);
+		// no need to change task description
+		setNewTaskDescription(localState.task);
+	};
+	//handle update
+	const handleUpdate = () => {};
+
+	//handle render task description
+	const renderTaskDescription = () => {};
+
+	//handle start
+	const handleStart = () => {};
+
+	//handle pause
+	const handlePause = () => {};
+
+	//handle delete
+	const handleDelete = () => {};
+
+	//handle render buttons
+	const handleRenderButtons = () => {};
+
 	return (
 		<div className="bg-white p-4 rounded-md text-black shadow-lg flex flex-col md:flex-row md:items-center justify-between">
 			<div className="md:space-x-2 space-y-2 md:space-y-0">
