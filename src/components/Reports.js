2afd11c407ae1af3d596a813468ef02fe93aa9df
Helena.Han <hlyhelena37@gmail.com>
2024-01-09 23:13:15 +1100

    feat: format time

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -5,6 +5,7 @@ import { getAuth } from 'firebase/auth';
 import app from '../firebase/config';
 import { collection, getFirestore, onSnapshot, query, where } from 'firebase/firestore';
 import Task from './Task';
+import { startOfWeek, endOfDay, startOfMonth, endOfMonth, isWithinInterval, addMilliseconds, format } from 'date-fns';
 
 //create auth instance
 const auth = getAuth(app);
@@ -33,17 +34,55 @@ function Reports() {
 
 					//onSnapshot
 					const unSubscribe = onSnapshot(firebaseQuery, (querySnapshot) => {
-						//fetch data from firestore
-						setTasks(
-							querySnapshot.docs.map((doc) => {
-								return {
-									...doc.data(),
-									id: doc.id,
-									date: new Date(doc.data().startTime).toISOString(),
-								};
-							})
-						);
+						//if have data in the store
+						if (querySnapshot.docs.length) {
+							let weekTotal = 0;
+							let monthTotal = 0;
+							let total = 0;
+							const now = new Date();
+							const weekStart = startOfWeek(now);
+							const weekEnd = endOfDay(now);
+
+							const monthStart = startOfMonth(now);
+							const monthEnd = endOfMonth(now);
+
+							//Get all date from tasks
+							querySnapshot.docs.forEach((doc) => {
+								const data = doc.data();
+								const taskStartTime = new Date(data.startTime);
+								const taskDuration = data.totalTime || 0;
+								if (isWithinInterval(taskStartTime, { start: weekStart, end: weekEnd })) {
+									weekTotal += taskDuration;
+								}
+								if (
+									isWithinInterval(taskStartTime, {
+										start: monthStart,
+										end: monthEnd,
+									})
+								) {
+									monthTotal += taskDuration;
+								}
+								total += taskDuration;
+							});
+
+							//update time state
+							setThisWeekTotal(weekTotal);
+							setThisMonthTotal(monthTotal);
+							setTotalTime(total);
+
+							//fetch data from firestore
+							setTasks(
+								querySnapshot.docs.map((doc) => {
+									return {
+										...doc.data(),
+										id: doc.id,
+										date: new Date(doc.data().startTime).toISOString(),
+									};
+								})
+							);
+						}
 					});
+					return unSubscribe;
 				} else {
 					setError('Please sign in to continue');
 					setLoading(false);
@@ -60,6 +99,12 @@ function Reports() {
 			}
 		};
 	}, []);
+
+	//format time
+	const formatTime = (time) => {
+		const date = addMilliseconds(new Date(0), time);
+		return format(date, 'HH:mm:ss');
+	};
 	return (
 		<div className="min-h-screen bg-gradient-to-r from-green-400 to-blue-500">
 			<div className="container mx-auto px-4 py-10">
@@ -84,15 +129,15 @@ function Reports() {
 				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-center mb-8">
 					<div className="bg-gradient-to-r from-green-400 to-blue-500 p-4 rounded-md text-white shadow-lg">
 						<h2 className="text-lg font-semibold">This Week</h2>
-						<p className="text-2xl font-bold">20</p>
+						<p className="text-2xl font-bold">{formatTime(thisWeekTotal)}</p>
 					</div>
 					<div className="bg-gradient-to-r from-purple-400 to-pink-500 p-4 rounded-md text-white shadow-lg">
 						<h2 className="text-lg font-semibold">This Month</h2>
-						<p className="text-2xl font-bold">10</p>
+						<p className="text-2xl font-bold">{formatTime(thisMonthTotal)}</p>
 					</div>
 					<div className="bg-gradient-to-r from-red-400 to-yellow-500 p-4 rounded-md text-white shadow-lg">
-						<h2 className="text-lg font-semibold">Total</h2>
-						<p className="text-2xl font-bold">30</p>
+						<h2 className="text-lg font-semibold">Total Time</h2>
+						<p className="text-2xl font-bold">{formatTime(totalTime)}</p>
 					</div>
 				</div>
 
